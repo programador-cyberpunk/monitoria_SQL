a4e123259030f099d5b295a9462eb2400c64d1bf
hugo <86883067+programador-cyberpunk@users.noreply.github.com>
2025-06-05 13:10:44 -0300

    Add files via upload

---
+++ stored_procedure.sql
@@ -0,0 +1,10 @@
+use bancodemo2
+GO
+
+CREATE PROCEDURE pesquisa
+ @campoBusca VARCHAR(20) AS
+ SELECT codigo ,descricao FROM tabela
+ WHERE descricao = @campoBusca
+ 
+ EXECUTE pesquisa
+ DROP PROCEDURE pesquisa
